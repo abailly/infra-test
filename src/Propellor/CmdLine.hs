cae4dde86c8061d3718da05d3475ae1ac53b224f
Arnaud Bailly <arnaud.oqube@gmail.com>
2014-08-31 14:51:09 +0200

    propellor spin

--- src/Propellor/CmdLine.hs
+++ src/Propellor/CmdLine.hs
@@ -217,7 +217,7 @@ spin hn hst = do
 		, "else " ++ intercalate " && "
 			[ "cd " ++ localdir
 			, "if ! test -x ./propellor; then make deps build; fi"
-			, "PROPELLOR_DEBUG=1 ./propellor --boot " ++ hn
+			, "./propellor --boot " ++ hn
 			]
 		, "fi"
 		]
