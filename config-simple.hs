15acc49dca5b42081a0acd61aa41721299da12a0
Arnaud Bailly <arnaud.oqube@gmail.com>
2014-09-26 12:38:01 +0200

    propellor spin

--- config-simple.hs
+++ config-simple.hs
@@ -6,6 +6,7 @@ import Propellor.CmdLine
 -- import Propellor.Property.Scheduled
 -- import qualified Propellor.Property.File as File
 import qualified Propellor.Property.Apt as Apt
+import qualified Propellor.Property.Apache as Apache
 -- import qualified Propellor.Property.Network as Network
 --import qualified Propellor.Property.Ssh as Ssh
 import qualified Propellor.Property.Cron as Cron
@@ -31,7 +32,28 @@ hosts =
           -- configure user build
           & User.accountFor "build"
 
-	-- A generic webserver in a Docker container.
+        , host "92.243.3.60"
+          & Apt.serviceInstalledRunning "apache2"
+          & Apache.modEnabled "ssl"
+          & (Apache.siteEnabled "atdd.io" $ apachecfg "atdd.io" NoSSL
+		[ "  DocumentRoot /srv/nono-data/atdd.io/_site"
+		, "  <Directory /srv/nono-data/atdd.io/_site>"
+		, "    Options Indexes FollowSymlinks MultiViews"
+		, "    AllowOverride None"
+		, "    Order allaw,deny"
+		, "  </Directory>"
+		, ""
+		, "  ScriptAlias /cgi-bin/ /usr/lib/cgi-bin/"
+		, "  <Directory /usr/lib/cgi-bin>"
+		, "    SetHandler cgi-script"
+		, "    Options +ExecCGI -MultiViews +SymLinksIfOwnerMatch"
+		, "  </Directory>"
+                , " ErrorLog ${APACHE_LOG_DIR}/atdd.io.error.log"
+                , " LogLevel warn"
+                , " AccessLog ${APACHE_LOG_DIR}/atdd.io.access.log combined"
+		])
+
+          -- A generic webserver in a Docker container.
 	, Docker.container "webserver" "joeyh/debian-stable"
 		& os (System (Debian Stable) "amd64")
 		& Apt.stdSourcesList
@@ -42,3 +64,54 @@ hosts =
 	-- add more hosts here...
 	--, host "foo.example.com" = ...
 	]
+
+
+data VHostSSL = NoSSL
+              | WithSSL
+              deriving (Eq,Show,Read)
+                       
+-- | Configuration for apache virtual host
+-- stolen from JoeysSites
+apachecfg :: HostName             -- ^Host's name
+          -> VHostSSL            -- ^Configure SSL virtual host?
+          -> Apache.ConfigFile   -- ^Configuration file to modify
+          -> Apache.ConfigFile
+apachecfg hn withSSL middle
+  | withSSL == WithSSL = vhost NoSSL  ++ vhost WithSSL
+  | otherwise         = vhost NoSSL
+  where
+	vhost ssl = 
+		[ "<VirtualHost *:"++show port++">"
+		, "  ServerAdmin arnaud@foldlabs.com"
+		, "  ServerName "++hn++":"++show port
+		]
+		++ mainhttpscert ssl
+		++ middle ++
+		[ ""
+		, "  ErrorLog /var/log/apache2/error.log"
+		, "  LogLevel warn"
+		, "  CustomLog /var/log/apache2/access.log combined"
+		, "  ServerSignature On"
+		, "  "
+		, "  <Directory \"/usr/share/apache2/icons\">"
+		, "      Options Indexes MultiViews"
+		, "      AllowOverride None"
+		, "      Order allow,deny"
+		, "      Allow from all"
+		, "  </Directory>"
+		, "</VirtualHost>"
+		]
+	  where
+		port = case withSSL of
+                        NoSSL   -> 80 :: Int
+                        WithSSL -> 443 :: Int
+
+mainhttpscert :: VHostSSL -> Apache.ConfigFile
+mainhttpscert NoSSL   = []
+mainhttpscert WithSSL = 
+	[ "  SSLEngine on"
+	, "  SSLCertificateFile /etc/ssl/certs/web.pem"
+	, "  SSLCertificateKeyFile /etc/ssl/private/web.pem"
+	, "  SSLCertificateChainFile /etc/ssl/certs/startssl.pem"
+	]
+		
