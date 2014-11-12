e74ae37f003f65b2518b2a26cc78d74526328080
Arnaud Bailly <arnaud.oqube@gmail.com>
2014-11-12 11:17:10 +0800

    propellor spin

--- src/Propellor/Property/Fig.hs
+++ src/Propellor/Property/Fig.hs
@@ -21,7 +21,7 @@ import System.Posix.Files
 installed :: Property
 installed = prop `requires` Docker.installed
   where
-	prop = check (doesFileExist "/usr/local/bin/fig") $
+	prop = check (not <$> doesFileExist "/usr/local/bin/fig") $
 		   combineProperties "fig installed"
 			   [ scriptProperty [ "curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig" ]
 			   , File.mode "/usr/local/bin/fig" (combineModes  (ownerWriteMode:readModes ++ executeModes))
