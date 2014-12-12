bb95db99b1d8ef2cfafa6ea890516ebe0f32904e
Arnaud Bailly <arnaud.oqube@gmail.com>
2014-12-12 15:07:56 +0100

    propellor spin

--- src/Propellor/Property/Cabal.hs
+++ src/Propellor/Property/Cabal.hs
@@ -3,19 +3,23 @@ module Propellor.Property.Cabal where
 
 import Data.List
 import Propellor
+import Propellor.Property.User
 
 import Utility.SafeCommand
 
 type PackageName = String
 
 updated :: UserName -> Property
-updated user = property ("update cabal and all packages for user " ++ user) $
-			   ensureProperty $
-			   userScriptProperty user [ "cabal update" , "cabal install cabal-install"  ]
+updated user = property ("update cabal and all packages for user " ++ user) $ do
+                  home <- liftIO $ homedir user
+                  ensureProperty $ userScriptProperty user [ "cabal update"
+														   , "cabal install cabal-install"
+														   , "echo 'export PATH=$PATH:" ++ home ++ "/.cabal/bin' >> " ++ home ++ "/.bash_profile"
+														   ]
 		  
 -- |Install latest versions of the listed packages
 installed :: UserName -> [ PackageName ] -> Property
-installed user pkgs = prop `requires` updated user
+installed user pkgs = prop
   where
 	pkgList =  concat (intersperse " " (map shellEscape pkgs))
 	prop = property ("install packages " ++ pkgList) $ ensureProperty $ 
