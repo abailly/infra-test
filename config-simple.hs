88fef276ae93eb841bbe957544adc2c050eca26d
Willem van den Ende <willem@qwan.eu>
2015-03-05 13:48:20 +0000

    propellor spin

--- config-simple.hs
+++ config-simple.hs
@@ -32,27 +32,61 @@ main = defaultMain hosts
 hosts :: [Host]
 hosts =
 	[ host "lending.capital-match.com"
-        & File.dirExists "/etc/nginx/conf/ssl/"
+        & File.dirExists "/etc/nginx/conf/certs/"
 	& withPrivData (PrivFile "nginx-private-key") (Context "lending.capital-match.com")
 	  (\ getdata -> property "setting nginx private key"
-					$ getdata $ \ tok -> liftIO $ ((writeFile "/etc/nginx/conf/ssl/ssl.key" tok) >> return MadeChange) `catchIO` const (return FailedChange))
-	& "/etc/nginx/conf/ssl/ssl.key" `File.mode` combineModes [ownerWriteMode, ownerReadMode]
+					$ getdata $ \ tok -> liftIO $ ((writeFile "/etc/nginx/conf/certs/ssl.key" tok) >> return MadeChange) `catchIO` const (return FailedChange))
+	& "/etc/nginx/conf/certs/ssl.key" `File.mode` combineModes [ownerWriteMode, ownerReadMode]
 
 	& withPrivData (PrivFile "nginx-public-cert") (Context "lending.capital-match.com")
 	  (\ getdata -> property "setting nginx certificate chain"
 					$ getdata $ \ tok -> liftIO $ ((writeFile "/etc/nginx/conf/ssl-unified.crt" tok) >> return MadeChange) `catchIO` const (return FailedChange))
         & installLatestDocker
+        & User.accountFor "build"
+          & User.hasGroup "build" "docker"
+	  & Sudo.binaryEnabledFor "/usr/bin/docker" "build"
+ 		& Ssh.keyImported SshRsa "build" (Context "beta.capital-match.com")
+		  & File.containsLines "/home/build/.ssh/config"
+		  [ "Host bitbucket.org"
+				  , "\tUser git"
+				  , "\tHostname bitbucket.org"
+				  , "\tPreferredAuthentications publickey"
+				  , "\tIdentityFile \"/home/build/.ssh/id_rsa\""
+				  ]
+		  & Ssh.knownExternalHost "bitbucket.org" "build"
+		  & Ssh.authorizedKeys "build" (Context "beta.capital-match.com")
+
+          & File.hasContent "/home/build/startnginx.sh" 
+          ["#!/bin/sh"
+          ,"#set -x"
+          ,"#set -e"
+          , "if [-f /home/build/.app.cid ]; then"
+          ,"    docker kill $(cat /home/build/.app.cid)"
+          ,"    rm /home/build/.app.cid"
+          ,"  fi"
+          ,"  # run as build user"
+          ,"  docker run -d --cidfile=/home/build/.app.cid -p 8080:8080 -v /home/build/data:/data capital/app:latest"
+          ,"  if [ -f /home/build/.nginx.cid ]; then"
+          ,"    docker kill $(cat /home/build/.nginx.cid)"
+          ,"    rm /home/build/.nginx.cid"
+          ,"  fi"
+          ,"  # clone or pull nginx config as build user"
+          ,"  export NGINXCONF=/home/build/nginxconf/nginx"
+          ,"  if [ -d /home/build/nginxconf ]; then"
+          ,"    cd /home/build/nginxconf && git pull origin master"
+          ,"  else "
+          ,"    cd /home/build && git clone capital-match nginxconf"
+          ,"  fi","  docker run -d --cidfile=/home/build/.nginx.cid -p 80:80 -p 443:443 -v $NGINXCONF/nginx.conf:/etc/nginx/nginx.conf -v $NGINXCONF/sites-enabled:/etc/nginx/sites-enabled -v $NGINXCONF/certs:/etc/nginx/certs -v $NGINXCONF/logs:/var/log/nginx capital/nginx","fi"]
+		  & File.mode "/home/build/startnginx.sh" (combineModes  (ownerWriteMode:readModes ++ executeModes))
+      & File.ownerGroup "/home/build/startnginx" "build" "build"
+
         , host "beta.capital-match.com"
 	& Git.installed
 	& installLatestDocker
 	& Fig.installed
-        & File.dirExists "/registry"
 
-        -- TODO need new propellor version for `period`  https://github.com/joeyh/propellor/blob/master/config-simple.hs
-        -- & Docker.garbageCollected `period` Daily
-
-          -- configure user build
-          & User.accountFor "build"
+      -- configure user build
+      & User.accountFor "build"
 		  & User.hasGroup "build" "docker"
 		  & Ssh.keyImported SshRsa "build" (Context "beta.capital-match.com")
 		  & File.containsLines "/home/build/.ssh/config"
