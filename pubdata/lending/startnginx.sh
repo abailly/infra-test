207112f5b59f65039146fcc9a2f0a3f759fb684c
Willem van den Ende <willem@qwan.eu>
2015-03-06 15:22:43 +0000

    propellor spin

--- pubdata/lending/startnginx.sh
+++ pubdata/lending/startnginx.sh
@@ -6,16 +6,11 @@ if [ -f /home/build/.app.cid ]; then
     rm /home/build/.app.cid
 fi
 # run as build user
-docker run -d --cidfile=/home/build/.app.cid -p 8080:8080 capital/app:latest
+docker run -d --cidfile=/home/build/.app.cid -p 8080:8080 capitalmatch/app:latest
 if [ -f /home/build/.nginx.cid ]; then
    docker kill $(cat /home/build/.nginx.cid)
    rm /home/build/.nginx.cid
  fi
   # clone or pull nginx config as build user
-export NGINXCONF=/home/build/nginxconf/nginx
-#if [ -d /home/build/nginxconf ]; then
-#  cd /home/build/nginxconf && git pull origin master
-#else 
-#  cd /home/build && git clone capital-match nginxconf
-#fi
-docker run -d --cidfile=/home/build/.nginx.cid -p 80:80 -p 443:443 -v $NGINXCONF/sites-enabled:/etc/nginx/sites-enabled -v /etc/nginx/certs:/etc/nginx/certs -v $NGINXCONF/logs:/var/log/nginx capital/nginx
+export NGINXCONF=/home/build/nginxconf/
+docker run -d --cidfile=/home/build/.nginx.cid -p 80:80 -p 443:443 -v $NGINXCONF/sites-enabled:/etc/nginx/sites-enabled -v /etc/nginx/certs:/etc/nginx/certs -v $NGINXCONF/logs:/var/log/nginx capitalmatch/nginx
