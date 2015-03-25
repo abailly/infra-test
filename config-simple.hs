ce1255e83cd700763ff3761aa6d4ed4778cd983f
Willem van den Ende <willem@qwan.eu>
2015-03-25 23:56:32 +0000

    propellor spin

--- config-simple.hs
+++ config-simple.hs
@@ -110,7 +110,7 @@ hosts =
           , "\tIdentityFile \"/home/admin/.ssh/id_rsa\""
           ]
           & Ssh.knownExternalHost "bitbucket.org" "admin"
-          -- fig
+          -- FIG
           & Fig.installed
           & firewallHttpsDockerSsh
 
@@ -176,8 +176,9 @@ devhost = propertyList "creating devserver configuration" $ props
           & File.containsLine "/home/build/.bash_profile" "alias g=git"
           & Git.configuredUser "build" "Igitur Ventures Ltd." "igitur@igitur.io"
           & Ssh.knownExternalHost "bitbucket.org" "build"
+          & Ssh.knownExternalHost "beta.capital-match.org" "build"
           & Ssh.authorizedKeys "build" (Context "dev")
-          & Git.cloned "build" "ssh://build@128.199.237.250/~/capital-match" "/home/build/app" (Just "master")
+          & Git.cloned "build" "ssh://build@beta.capital-match.org/~/capital-match" "/home/build/app" (Just "master")
           & File.hasPubContent "dev/app-git-config" "/home/build/app/.git/config"
           & installEmacs4Haskell "build"
           -- & configureEmacs "build"
