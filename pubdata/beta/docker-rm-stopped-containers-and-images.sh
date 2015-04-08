1424f4d10323f79165d34287174bf0b8360c855a
Willem van den Ende <willem@qwan.eu>
2015-04-08 15:56:29 +0100

    propellor spin

--- pubdata/beta/docker-rm-stopped-containers-and-images.sh
+++ pubdata/beta/docker-rm-stopped-containers-and-images.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 docker ps -a | awk '{print $1}' | xargs --no-run-if-empty docker rm
 docker images | awk '{print $3}' | xargs --no-run-if-empty docker rmi
-rm /home/build/app/images/*.uuid
+
+#restart ci
+cd /home/build/ci
+fig stop
+fig build
+fig up -d
