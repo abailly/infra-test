6385e824ffd59116e35bcccff558f9f96e0dc921
Willem van den Ende <willem@qwan.eu>
2015-05-19 15:06:56 +0100

    Remove non-latest images

--- pubdata/beta/docker-rm-stopped-containers-and-images.sh
+++ pubdata/beta/docker-rm-stopped-containers-and-images.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 docker ps -a | awk '{print $1}' | xargs --no-run-if-empty docker rm
 #remove all unused images, except those tagged as latest, to speed up the first build after cleaning
-docker images | sed '/latest/d' | awk '{print $3}' | xargs --no-run-if-empty docker rmi
+docker images | grep latest | awk '{ printf("%s\n",$3) }' | sort > latest.txt && cat latest.txt
+docker images | grep -v IMAGE | awk '{ printf("%s\n",$3) }' | sort > all.txt && cat all.txt
+comm -3 all.txt latest.txt | docker rmi
 
 #restart ci
 cd /home/build/ci
