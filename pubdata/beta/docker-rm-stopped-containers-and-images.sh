48cf2fb31a4bccb287ea979c6d7aeda06a232d01
Willem van den Ende <willem@qwan.eu>
2015-05-19 15:09:51 +0100

    propellor spin

--- pubdata/beta/docker-rm-stopped-containers-and-images.sh
+++ pubdata/beta/docker-rm-stopped-containers-and-images.sh
@@ -3,7 +3,7 @@ docker ps -a | awk '{print $1}' | xargs --no-run-if-empty docker rm
 #remove all unused images, except those tagged as latest, to speed up the first build after cleaning
 docker images | grep latest | awk '{ printf("%s\n",$3) }' | sort > latest.txt && cat latest.txt
 docker images | grep -v IMAGE | awk '{ printf("%s\n",$3) }' | sort > all.txt && cat all.txt
-comm -3 all.txt latest.txt | docker rmi
+comm -3 all.txt latest.txt | xargs docker rmi
 
 #restart ci
 cd /home/build/ci
